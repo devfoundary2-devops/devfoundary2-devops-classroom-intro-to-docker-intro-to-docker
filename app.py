7a2ecbaf66af69974df765e44f39a8f7d1b9dc23
John Afariogun <afariogun.john@yahoo.com>
2025-09-11 02:19:25 +0100

    fix: implemented fix for docker compose

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 import redis
 
 app = FastAPI()
-r = redis.Redis(host="redis", port=6379)
+r = redis.Redis(host="redis", port=6379, decode_responses=True)
 
 
 @app.get("/cache/{key}")
