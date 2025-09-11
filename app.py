3603be35359b7cc26cd272df221ac2957a66dae5
John Afariogun <afariogun.john@yahoo.com>
2025-09-11 02:38:27 +0100

    feat: added postgresql integration in the application

--- app.py
+++ app.py
@@ -1,8 +1,47 @@
 from fastapi import FastAPI
 import redis
+import psycopg2
+import os
 
 app = FastAPI()
 r = redis.Redis(host="redis", port=6379, decode_responses=True)
+# Database connection settings
+
+DB_HOST = os.getenv("DB_HOST", "db")   # "db" is the service name from docker-compose
+DB_NAME = os.getenv("POSTGRES_DB", "demo")
+DB_USER = os.getenv("POSTGRES_USER", "demo")
+DB_PASS = os.getenv("POSTGRES_PASSWORD", "password")
+DB_PORT = os.getenv("DB_PORT", "5432")
+
+def get_connection():
+    return psycopg2.connect(
+        host=DB_HOST,
+        database=DB_NAME,
+        user=DB_USER,
+        password=DB_PASS,
+        port=DB_PORT
+    )
+@app.post('/users')
+def create_users():
+    conn = get_connection()
+    cur = conn.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, name TEXT);")
+    cur.execute("INSERT INTO users (name) VALUES ('Alice'), ('Bob') ON CONFLICT DO NOTHING;")
+    conn.commit()
+    return {"sucess": "ok"}
+
+@app.get("/users")
+def get_users():
+    conn = get_connection()
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM users;")
+    rows = cur.fetchall()
+
+    cur.close()
+    conn.close()
+
+    return {"users": rows}
+
 
 
 @app.get("/cache/{key}")
